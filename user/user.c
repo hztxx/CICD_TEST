3bdfde68736d681353047c4162b6836ee49374b2
Miracle <1135164382@qq.com>
2026-07-03 00:40:42 +0800

    feat: 新增违规命名变量Test_abc，测试clang-tidy流水线拦截

--- user/user.c
+++ user/user.c
@@ -11,7 +11,7 @@ void user_init(void)
 {
     // 局部变量 纯小写下划线
     uint16_t buf_index = 0;
-    int Test_abc;
+    int HHT_zx;
     // 清空全局数据
     for(buf_index = 0; buf_index < USER_DATA_MAX_LEN; buf_index++)
     {
